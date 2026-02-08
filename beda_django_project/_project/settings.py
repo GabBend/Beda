8b8a3f275f9e0ca0d73cacd263e02a6431917d30
Gabriela Bendová <glbendovi@gmail.com>
2026-02-08 08:40:24 +0100

    setting time

--- beda_django_project/_project/settings.py
+++ beda_django_project/_project/settings.py
@@ -103,13 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europa/Prague'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
