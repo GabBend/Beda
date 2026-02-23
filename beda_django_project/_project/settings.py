23f3e5724e31592fa912e595d91ad27ce9770a46
Gabriela Bendová <glbendovi@gmail.com>
2026-02-23 12:39:25 +0100

    cache v setings.py

--- beda_django_project/_project/settings.py
+++ beda_django_project/_project/settings.py
@@ -125,8 +125,8 @@ STATICFILES_DIRS = [
 ]
 
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "unique-divilidi",
-    }
+    # "default": {
+    #     "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    #     "LOCATION": "unique-divilidi",
+    # }
 }
