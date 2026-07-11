9823f4870aa4d6ae88e1f7cc0404ce49512522e4
Gabriela Bendová <glbendovi@gmail.com>
2026-07-11 15:27:33 +0200

    Update sitemaps.py

--- beda_django_project/blog/sitemaps.py
+++ beda_django_project/blog/sitemaps.py
@@ -7,7 +7,18 @@ class StaticViewSitemap(Sitemap):
     changefreq = "monthly"
 
     def items(self):
-        return ["index", "photo", "contact"]
+        return [
+            "index",
+            "zakazkova_vyroba",
+            "plachty_na_miru",
+            "autostany",
+            "pergoly_a_pristresky",
+            "teepee_a_taborove_vyrobky",
+            "atypicke_vyrobky",
+            "opravy_technickych_textilii",
+            "photo",
+            "contact",
+        ]
 
     def location(self, item):
         return reverse(item)
